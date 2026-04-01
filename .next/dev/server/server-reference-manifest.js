61217d7e66add45ce613e7f02ceb13714b58f1e7
itsahadul99 <itsahadul999@gmail.com>
2026-04-01 21:56:09 +0600

    Remove .next folder from repo

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"t7sSg0glsu91MVixWk3VKw+WB6mlxC+ZkamT2Ac5A8I=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_0b~n5mw._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_05gap3k._.js","static/chunks/node_modules_0f9ws7n._.js"],
-    source: "dynamic"
-});
